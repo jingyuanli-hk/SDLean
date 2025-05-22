400c2aed2bcd0e97c5b0903c312c338b1a6ad683
Li Jingyuan <jingyuanli@ln.edu.hk>
2025-05-22 22:40:13 +0800

    Update FSD.MultiDim.lean

--- FSD.MultiDim.lean
+++ FSD.MultiDim.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2025 Li Jingyuan . All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Li Jingyuan
-Extension for N-dimensional FSD: GitHub Copilot
+Extension for N-dimensional FSD
 -/
 import Mathlib.Analysis.Calculus.Deriv.Basic
 
